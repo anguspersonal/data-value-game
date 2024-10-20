45a63c3ddcbf5ecacba98d9d3af4f3335f30906b
Angus Hally Anmut <angus.hally@gmail.com>
2024-10-20 13:59:20 +0100

    Moved header and Footer from main.jsx to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import './App.css'
 import GameBoard from './components/GameBoard.jsx'
-
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
     <div>
+      <Header />
       <GameBoard />
+      <Footer />
     </div>
   )
 }
